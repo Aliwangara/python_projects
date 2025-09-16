2f02a836dc60d0c542a7783cbc5fc912805e99c8
Ali Wangara <Aliwangara63@gmail.com>
2025-09-16 14:49:42 +0300

    started a solo project currency coverter using api to check for available currencies

---
+++ solo_projects/currency_converter/currency.py
@@ -0,0 +1,21 @@
+from currency_class import Currency as c
+
+
+currency_api_key =  "https://app.exchangerate-api.com/activate/7d6a41c18e9c48ee4eb7c67f18"
+
+url = f"https://v6.exchangerate-api.com/v6/{currency_api_key}/latest/"
+print(url)
+
+# def main():
+#     currency()
+
+
+# def currency():
+#     amount = float(input("Enter Amount:  "))
+#     source_currency = input("Enter The currency you have now eg(USD, KSH): ")
+#     target_currency  = input("Which currency are you changing to: ")
+
+
+# if __name__ =="__main__":
+    main()
+
---
+++ solo_projects/currency_converter/currency_class.py
@@ -0,0 +1,7 @@
+
+
+class Currency:
+    def __init__(self, amount,from_currency,to_currency):
+        self.amount = amount
+        self.from_currency = from_currency
+        self.to_currency = to_currency
