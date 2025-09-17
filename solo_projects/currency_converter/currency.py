854b34b82892a9f8be4ca1b6f477e222fc8291cb
Ali Wangara <Aliwangara63@gmail.com>
2025-09-17 12:27:53 +0300

    created a currency converter function

--- solo_projects/currency_converter/currency.py
+++ solo_projects/currency_converter/currency.py
@@ -8,28 +8,32 @@ url = f'https://v6.exchangerate-api.com/v6/{currency_api_key}/latest/USD'
 
 response = r.get(url)
 data = response.json()
-new_data = data.get('conversion_rates').keys()
-# print(data)
-print("We have these currencies available: ") 
-formatted_data = tuple(new_data)
-
-print (data)
+new_data = data.get('conversion_rates')
 
 
-# # def main():
-# #     currency()
+
+def main():
+    currency()
 
 
-# def currency():
-#     amount = float(input("Enter Amount:  "))
-#     source_currency = input("Enter The currency you have now eg(USD, KSH): ")
-#     target_currency  = input("Which currency are you changing to: ")
+def currency():
+    amount = float(input("Enter Amount:  "))
+    source_currency = input("Enter The currency you have now eg(USD, KSH): ").upper()
+    target_currency  = input("Which currency are you changing to: ").upper()
 
-#     if source_currency and target_currency in 
+    if source_currency not in new_data and target_currency not in new_data:
+           print("Invalid Currency")
+           return
+    amount_in_usd = amount /new_data[source_currency]
+    converted_currency = amount_in_usd * new_data[target_currency]
+    
+    print(f"{converted_currency}")
 
+def save_converted_currency():
+     
     
 
 
-# if __name__ =="__main__":
-#     main()
+if __name__ =="__main__":
+    main()
 
