eefa60cf01ab5f5aa0c58fbdc9b3c20a615bbdf6
Ali Wangara <Aliwangara63@gmail.com>
2025-09-30 16:53:39 +0300

    Finished creating the system

--- python_practicses/inventory_system.py
+++ python_practicses/inventory_system.py
@@ -56,58 +56,102 @@
 # For sorting by total value: sorted(items, key=lambda x: x['quantity']*x['price'], reverse=True)
 
 
-item_list= []
+item_list = []
+
+
 def add_item():
     item_name = input("Enter name of the item:    ").lower()
     item_quantity = int(input("Enter Item quantity:   "))
     price = float(input("Enter price of the item:   "))
 
     item_info = {
-        "name":item_name,
-        "quantity":item_quantity,
-        "price":price
+        "name": item_name,
+        "quantity": item_quantity,
+        "price": price
     }
     item_list.append(item_info)
 
-add_item()
 
 def view_items():
     for item in item_list:
+        
         print(f"You picked {item['name']}\nQuantity: {item["quantity"]} Price per item is "
-               f"{item['price']}\nTotal is: {item['quantity'] * item['price'] :.2f}") 
-
+              f"{item['price']}\nTotal is: {item['quantity'] * item['price']:.2f}")
 view_items()
 
 def search_item():
-    search_input = input("Enter name of the item you are looking for:   ").lower()
+    search_input = input(
+        "Enter name of the item you are looking for:   ").lower()
     for item in item_list:
         if search_input in item["name"]:
             print(f"{search_input} is available. Costing {item['price']} each")
         else:
             print(f"{search_input} Isn't available")
 
-search_item()
 
 def most_valuable_item():
-    valuable_item = max(item_list, key=lambda i:i['price'] * i['quantity'])
+    valuable_item = max(item_list, key=lambda i: i['price'] * i['quantity'])
     print(f"Most valuable Item is: {valuable_item}")
-    
 
-most_valuable_item()
 
 def sorting_items():
 
-  sorting = sorted(item_list, key=lambda s:(s['name'], s['quantity'] * s['price']))
-  print(f"Sorted items: {sorting}")
+    sorting = sorted(item_list, key=lambda s: (
+        s['name'], s['quantity'] * s['price']))
+    print(f"Sorted items: {sorting}")
 
-sorting_items()
 
 def increase_decrease():
-    for new_item in item_list:
-        if new_item in item_list:
-              
-        
+    print("==== Items Available ====")
+    for i, list_number in enumerate(item_list, 1):
+        print(f"{i}. {list_number}")
+    list_item_input = int(
+        input("Select  the number of item you want to increase it's quantity:  ")) - 1
+
+    if 0 <= list_item_input <= len(item_list):
+        inc_dec = input("Which operation do you want to perform (- or +):   ")
+        number_input = int(
+            input("Enter quantity you want to increase or decrease:  "))
+
+        if inc_dec == "+":
+            item_list[list_item_input]["quantity"] += number_input
+        elif inc_dec == "-":
+            item_list[list_item_input]["quantity"] -= number_input
+        else:
+            print("Invalid operation")
+
+        print(
+            f"Updated: {item_list[list_item_input]['name']} now has quantity {item_list[list_item_input]['quantity']}")
+    else:
+        print("Invalid selection!")
 
 
+while True:
+    print("1. add item")
+    print("2. view items")
+    print("3. search item")
+    print("4. most valuable item ")
+    print("5. sorting items ")
+    print("6. increase decrease")
+    print("7. Exit")
 
-increase_decrease()
+    option_input = int(input("Select the number of input you want to use:  "))
+
+    if option_input == 1:
+        add_item()
+    elif option_input == 2:
+        view_items()
+    elif option_input == 3:
+        search_item()
+    elif option_input == 4:
+        most_valuable_item()
+    elif option_input == 5:
+        sorting_items()
+    elif option_input == 6:
+        increase_decrease()
+    elif option_input == 7:
+        print("== These are the items available ==")
+        print(f"{item_list}")
+        break
+    else:
+        print("Please select the available numbers")
