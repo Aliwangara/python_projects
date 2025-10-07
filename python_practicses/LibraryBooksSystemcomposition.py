475e9b4a79fd55577fc496d8fe6ae27cfc56ca47
Ali Wangara <Aliwangara63@gmail.com>
2025-10-07 18:40:54 +0300

    Worked with composition advanced: list,loops

--- python_practicses/LibraryBooksSystemcomposition.py
+++ python_practicses/LibraryBooksSystemcomposition.py
@@ -46,3 +46,53 @@
 # Add the books to the library.
 
 # Display all books and total count.
+
+
+class Book:
+    def __init__(self,title, author):
+        self.__title  = title
+        self.__author = author
+
+    def get_author(self):
+        return self.__author
+    def get_title(self):
+        return self.__title
+    def book_info(self):
+        print(f"Book: {self.__title}, By: {self.__author}")
+
+class Library:
+    def __init__(self,name):
+       self.__name = name
+       self.__books = []
+    def add_books(self,book):
+        self.__books.append(book)
+    def show_books(self):
+        print(f"\nLibrary {self.__name}")
+        for i in self.__books:
+            i.book_info()
+    def total_books(self):
+        print(f"Total Books: {len(self.__books)}")
+
+book1 = Book("Python Crash Course", "Eric Matthes")
+book2 = Book("Clean Code","Robert C. Martin" )
+book3 = Book("Atomic Habits", "James Clear")
+
+
+library = Library("City Library")
+
+library.add_books(book1)
+library.add_books(book2)
+library.add_books(book3)
+
+library.show_books()
+library.total_books()
+
+
+
+
+
+
+
+
+
+
