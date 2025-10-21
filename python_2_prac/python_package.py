f49f823fc97e908d151bb7c08060f3c99fb44dfd
Ali Wangara <Aliwangara63@gmail.com>
2025-10-21 11:27:33 +0300

    worked with python packages management importing etc

---
+++ python_2_prac/python_package.py
@@ -0,0 +1,14 @@
+# Create a module named string_utils.py with these functions:
+
+# to_uppercase(text) – converts a string to uppercase
+
+# word_count(text) – counts how many words are in a string
+
+# Then, in a separate file main.py, import your module and test both functions.
+
+
+import string_utils 
+
+print(string_utils.to_uppercase("Ali"))
+print(string_utils.word_count("I am a student"))
+
---
+++ python_2_prac/string_utils.py
@@ -0,0 +1,10 @@
+
+def to_uppercase(text):
+    return text.upper()
+
+
+def word_count(text):
+    word = text.split() 
+    return len(word)
+
+
