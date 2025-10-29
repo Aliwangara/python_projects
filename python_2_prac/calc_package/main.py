b83150f332a663f17715d538d74e8f384cc6a141
Ali Wangara <Aliwangara63@gmail.com>
2025-10-29 19:01:47 +0300

    worked on  multiple modules importation

---
+++ python_2_prac/calc_package/main.py
@@ -0,0 +1,8 @@
+from addition import add
+from subtraction import subtract
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Sum: ", add(num1,num2))
+print("Difference: ", subtract(num1,num2))
