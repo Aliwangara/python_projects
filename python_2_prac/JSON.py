9848608d98a7a7c4489f6c05afaa72113a0a43a8
Ali Wangara <Aliwangara63@gmail.com>
2025-10-30 12:34:48 +0300

    worked with json. dump to save data to a json file, load getting data, dumps converting to a string

--- python_2_prac/JSON.py
+++ python_2_prac/JSON.py
@@ -1,1 +1,40 @@
-import JSON
+import json
+import os
+
+students_file = "students.json"
+
+name = input("Enter name: ")
+age = int(input("Enter age: "))
+marks = []
+
+print("=== Enter marks for 3 subjects ===")
+for i in range(3):
+    marks_input = float(input(f"Enter mark {i+1}: "))
+    marks.append(marks_input)
+
+
+student_records = {
+    "name":name,
+    "age": age,
+    "marks": marks
+}
+
+students_list =[]
+
+students_list.append(student_records)
+
+with open(students_file, 'w') as f:
+    json.dump(students_list,f,indent=4)
+    f.write('\n')
+    
+if os.path.exists(students_file) == False:
+        print("Folder doesn't exist ")
+elif os.path.getsize(students_file) == 0:
+        print("Student file is empty")
+else:
+        with open(students_file, "r") as f:
+            data = json.load(f)
+            print("\n✅ All Students:")
+            print(json.dumps(data, indent=4))
+        
+  
