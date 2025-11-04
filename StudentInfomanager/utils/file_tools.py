ed552463cbc298615d1d8b32eb205b6d4da7d8a4
Ali Wangara <Aliwangara63@gmail.com>
2025-11-04 20:52:50 +0300

    made a function that adds students and displays the data. created a python file that saves the added data in a json file using file handling method. Imported the function into this file

--- StudentInfomanager/utils/file_tools.py
+++ StudentInfomanager/utils/file_tools.py
@@ -3,24 +3,25 @@ import os
 import json
 
 
+
 students_file = "students.json"
 
 student_data = main_file.student_data()
 
-# if not os.path.exists(students_file):
-#     with open(students_file,"r") as f:
-#         try:
-#             existing_data = json.load(f)
-#         except json.JSONDecodeError:
-#             existing_data = []
-# else:
-#     existing_data = []
+if not os.path.exists(students_file):
+    with open(students_file,"r") as f:
+        try:
+         reader = json.load(f)
+        except FileNotFoundError as e:
+           print(e)
+        else:
+           print(json.dumps(reader, indent=4))
 
-# existing_data.extend(student_data)
+       
 
 
-# with open(students_file,"w")as f:
-#     json.dump(student_data,f,indent=4)
+with open(students_file,"w")as f:
+    json.dump(student_data,f,indent=4)
 
 with open(students_file, 'r') as f:
     reader = json.load(f)
